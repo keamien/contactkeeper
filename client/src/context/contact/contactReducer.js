22567faf89528f9a5e7ea3bcfcc1294e2c36262e
Jon <jontroach@gmail.com>
2021-07-31 20:48:46 -0400

    Big ol commit

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -19,6 +19,39 @@ export default (state, action) => {
         ...state,
         contacts: [...state.contacts, action.payload],
       }
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      }
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null,
+      }
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter((contact) => contact.id !== action.payload),
+      }
+    case FILTER_CONTACTS:
+      return {
+        ...state,
+        filtered: state.contacts.filter((contact) => {
+          const regex = new RegExp(`${action.payload}`, 'gi')
+          return contact.name.match(regex) || contact.email.match(regex)
+        }),
+      }
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      }
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map((contact) => (contact.id === action.payload.id ? action.payload : contact)),
+      }
     default:
       return state
   }
